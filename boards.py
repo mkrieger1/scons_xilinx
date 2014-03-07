c60e3483bd92058ef6785330267297ccc37d4894
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-03-07 14:12:43 +0100

    added more devices

--- boards.py
+++ boards.py
@@ -18,6 +18,9 @@ class Device:
     def xst_part(self):
         return self.name+str(self.speed)+'-'+self.package
 
-susibo = Device('virtex5', 'xc5vlx30t', 'ff665', -2)
+
+susibo   = Device('virtex5',  'xc5vlx30t',  'ff665',  -2)
 syscore3 = Device('spartan6', 'xc6slx150t', 'fgg900', -3)
+htg_v6   = Device('virtex6',  'xc6vlx240t', 'ff1759', -2)
+htg_k7   = Device('kintex7',  'xc7k325t',   'fbg900', -2)
 
