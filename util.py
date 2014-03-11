bb5f7355a8c2241d5d7686c017c3558bd9817139
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-03-11 15:29:34 +0100

    added convenience function to generate file lists

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import os
+from fnmatch import fnmatch
 
 def get_suffix(filename):
     return os.path.splitext(str(filename))[1]
@@ -8,4 +9,17 @@ def replace_suffix(filename, suffix):
 
 def replace_path(filename, path):
     return os.path.join(path, os.path.basename(str(filename)))
+
+def get_file_list(path, pattern):
+    """Return filenames from `path` that match `pattern`.
+
+    `pattern` can be either a function or a string.
+    """
+    if isinstance(pattern, basestring):
+        ptn_str = pattern
+        pattern = lambda s: fnmatch(s, ptn_str)
+    join = os.path.join
+    isfile = os.path.isfile
+    return [join(path, f) for f in os.listdir(path)
+            if isfile(join(path, f)) and pattern(f)]
 
